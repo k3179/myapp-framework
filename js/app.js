25e025036e73d410dece17ce582c80755c855825
k3179 <yunkuangsu@gmail.com>
2019-08-27 17:02:41 +0800

    add app.js

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        document.addEventListener('pause', this.onDevicePause.bind(this), false);
+        document.addEventListener('resume', this.onDeviceResume.bind(this), false);
+    },
+
+    // device ready Event Handler
+    onDeviceReady: function() {
+        console.log('ready');
+    },
+    // device pause Event Handler
+    onDevicePause: function() {
+        console.log('pause');
+    },
+    // devide resume Event Handler
+    onDeviceResume: function() {
+        console.log('resume');
+    }
+
+};
