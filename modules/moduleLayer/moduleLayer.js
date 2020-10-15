4ad79c797341cc4cca569fd245efe281e7593a22
k3179 <yunkuangsu@gmail.com>
2020-10-15 10:18:06 +0800

    cntx-branch

--- modules/moduleLayer/moduleLayer.js
+++ modules/moduleLayer/moduleLayer.js
@@ -2,6 +2,13 @@ app.modules.moduleLayer = function(){}
 app.modules.moduleLayer.prototype = $.extend({},app.module.layer,{
     moduleType : 'layer',
     bindCustom : function(){
-
+        this.bindSubmit();
+    },
+    bindSubmit : function(){
+        var self = this;
+        $('form',this.$module).submit(function(){
+            console.log(self);
+            return false;
+        });
     }
 });
